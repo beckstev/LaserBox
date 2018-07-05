34621b8412cdece62539b7f3afc356e1a7310986
beckstev <beckstev@web.de>
2018-07-05 15:23:24 -0400

    modified Gaus2D.h

--- steven_test_analyse/Gaus2D.h
+++ steven_test_analyse/Gaus2D.h
@@ -7,14 +7,4 @@ Double_t Gaus2D( Double_t *cor, Double_t *par){
   return par[0] * TMath::Exp( -( x_comp**2 + y_comp**2) );
 }
 
-//Double_t Gaus2D(Double_t *x, Double_t *par)
-//{
-//if(par[2] > 0 && par[4] > 0)
-// {
-// double rx=(x[0]-par[1])/par[2];
-// double ry=(x[1]-par[3])/par[4];
-// return par[0]*TMath::Exp(-(rx*rx+ry*ry)/2.);
-// }
-//else
-// return 0.;
-//}
+
