29edcc0c4ca9a560cad9179b7482e650cdeded12
beckstev <beckstev@web.de>
2018-07-07 19:45:31 +0200

    edited create.sh, now it changes the name of root_6_fit_laser_test_vanilla.c in each new folder

--- laser_width/create.sh
+++ laser_width/create.sh
@@ -9,8 +9,12 @@ echo Creating folder with the name $name_folder
 
 mkdir ./$name_folder
 
-echo Copy root_6_fit_laser_test_vanilla into $name_folder
+echo Copy root_6_fit_laser_test_vanilla.c into $name_folder
 
 cp ./root_6_fit_laser_test_vanilla.c ./$name_folder
+
+echo Rename file root_6_fit_laser_test_vanilla.c to "${name_folder}_root_6_fit_laser.c"
+
+mv ./$name_folder/root_6_fit_laser_test_vanilla.c ./$name_folder/"${name_folder}_root_6_fit_laser.c"
 
 echo Finish. Have fun with $name_folder
