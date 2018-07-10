59d1d6974d85b33a7f281a999bbf5d284745f861
beckstev <beckstev@web.de>
2018-07-10 09:47:35 -0400

    changed create.sh and renamed root6_fit_laser_test_vanilla to root_6_fit_laser_beam

--- laser_width/create.sh
+++ laser_width/create.sh
@@ -9,12 +9,8 @@ echo Creating folder with the name $name_folder
 
 mkdir ./$name_folder
 
-echo Copy root_6_fit_laser_test_vanilla.c into $name_folder
+echo Copy root_6_fit_laser_beam.c into $name_folder
 
-cp ./root_6_fit_laser_test_vanilla.c ./$name_folder
-
-echo Rename file root_6_fit_laser_test_vanilla.c to "${name_folder}_root_6_fit_laser.c"
-
-mv ./$name_folder/root_6_fit_laser_test_vanilla.c ./$name_folder/"${name_folder}_root_6_fit_laser.c"
+cp ./root_6_fit_laser_beam.c ./$name_folder
 
 echo Finish. Have fun with $name_folder
