d957169b43302598308346256a69251f17dc4f2d
beckstev <beckstev@web.de>
2018-07-17 23:03:48 +0200

    Fit is not functional yet

--- ivcurves/schottky_function/result.py
+++ ivcurves/schottky_function/result.py
@@ -8,32 +8,43 @@ from uncertainties.unumpy import nominal_values as noms
 from uncertainties.unumpy import std_devs as stds
 from scipy.optimize import curve_fit
 from scipy import odr
-
+from array import array
+import time
 import ROOT as root
+
+root.gStyle.SetOptTitle(0)
+root.gStyle.SetOptFit(1)
+
 
 boltzmann_constant = const.physical_constants["Boltzmann constant"]
 electron_charge = const.physical_constants["elementary charge"]
+k_B = ufloat( boltzmann_constant[0], boltzmann_constant[2])
+e = ufloat( electron_charge[0], electron_charge[2])
+T = ufloat( 293.15, 2)
 
-def schottky_function(p, U):
-	k_B =  boltzmann_constant[0]
-	e = electron_charge[0]
-	T =  293.15
-	return I_L * ( np.exp( (e * U)/(k_B * T) -1 )
+def schottky_function(I_L, x):
+	return I_L * ( np.exp( x)  -1 )
 
-u_bias_vanilla, i_leak_vanilla_down   = np.genfromtxt('iv_data_hysterese.txt', unpack = True)
+u_bias_vanilla, i_leak_vanilla_down   = np.genfromtxt('I_V_curve_-30_200.txt', unpack = True)
 
-schottky_model = odr.Model(schottky_function)
-data = odr.RealData(u_bias_vanilla, i_leak_vanilla_down , sx= u_bias_vanilla * 0.02 , sy= i_leak_vanilla_up * 0.035)
+u_bias = unp.uarray( [u_bias_vanilla, np.abs(u_bias_vanilla) * 0.02 ])
 
-odr = odr.ODR(data, quad_model)
+schotty_argument = ( e* u_bias)/(T*k_B)
 
-out = odr.run()
 
+errorbar_plot_schottky = root.TGraphErrors( len(u_bias), array( 'f', noms(schotty_argument)), array( 'f', i_leak_vanilla_down), array( 'f',  stds(schotty_argument) ), array( 'f', np.abs(i_leak_vanilla_down) * 0.035 ) )
+
+
+schottky_fun = root.TF1('schottky_fun', '[0] * ( -exp(-x) + 1)', min( noms(schotty_argument) ), max( noms(schotty_argument) ))
+schottky_fun.SetParameter(0,0.12)
+errorbar_plot_schottky.Fit(schottky_fun)
+errorbar_plot_schottky.Draw()
+time.sleep(5)
 
 ############ Printint Measurepoints
 #
 #
-#u_bias = unp.uarray( [u_bias_vanilla, u_bias_vanilla * 0.0005 ])
+#
 #i_leak_up = unp.uarray( [i_leak_vanilla_up, 0.001 ])
 #i_leak_down = unp.uarray( [i_leak_vanilla_down,  0.001])
 #i_leak_second_up = unp.uarray( [i_leak_vanilla_second_up,  0.001])
