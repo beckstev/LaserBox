10a86f74b076ac89858c10cae1f04a882d69d943
Ramon Guerrero <guerrerr@purdue.edu>
2018-07-19 17:21:38 -0400

    normalization bash script

--- laser_width/first_position/normalized/trial/testfit.c
+++ laser_width/first_position/normalized/trial/testfit.c
@@ -1,12 +1,14 @@
-void testfit(TString dataname){
+#include<fstream>
+
+void testfit(TString dataname,Double_t height){
     
     //********************************************************************
     //**************************IMPORT DATA*******************************
     //********************************************************************
     
-    TFile *f = new TFile(dataname);
+    TString filename = dataname+".root";
 
-    TString filename = f->GetName();
+    TFile *f = new TFile(filename);
 
     TCanvas *c = (TCanvas*)f->Get("Canvas_1");
     TProfile2D *h_LaserProfile2D = (TProfile2D*)c->GetPrimitive("qMap_Ag_C0_V0");
@@ -28,24 +30,45 @@ void testfit(TString dataname){
 
     TH1D *h_NLaserProfile1DX = h_NLaserProfile2D->ProjectionX();
     h_NLaserProfile1DX->SetName("Column projection");
-    h_NLaserProfile1DX->SetTitle(filename);
+    h_NLaserProfile1DX->SetTitle(dataname);
 
     TH1D *h_NLaserProfile1DY = h_NLaserProfile2D->ProjectionY();
     h_NLaserProfile1DY->SetName("Row projection");
-    h_NLaserProfile1DY->SetTitle(filename);
+    h_NLaserProfile1DY->SetTitle(dataname);
     
 
-    TF1 *g_NLaserProfile1DX = new TF1("g_NLaserProfile1DX","gaus",18,26);
+    TF1 *g_NLaserProfile1DX = new TF1("g_NLaserProfile1DX","gaus",19,26);
     TF1 *g_NLaserProfile1DY = new TF1("g_NLaserProfile1DY","gaus",40,52);
 
     g_NLaserProfile1DX->SetNpx(10000);
     g_NLaserProfile1DY->SetNpx(10000);
+
     //********************************************************************
     //*******************************FIT**********************************
     //********************************************************************
 
-    h_NLaserProfile1DX->Fit("g_NLaserProfile1DX");
-    h_NLaserProfile1DY->Fit("g_NLaserProfile1DY");
+    h_NLaserProfile1DX->Fit("g_NLaserProfile1DX","Q");
+    h_NLaserProfile1DY->Fit("g_NLaserProfile1DY","Q");
+
+    //********************************************************************
+    //**************************SAVE PARAMETERS***************************
+    //********************************************************************
+
+    Double_t parX[3];
+    Double_t parY[3];
+
+    g_NLaserProfile1DX->GetParameters(parX);
+    g_NLaserProfile1DY->GetParameters(parY);
+
+    Double_t *errparX= new Double_t(3);
+    Double_t *errparY= new Double_t(3);
+
+    errparX =(Double_t*) g_NLaserProfile1DX->GetParErrors();
+    errparY =(Double_t*) g_NLaserProfile1DY->GetParErrors();
+
+    ofstream sigma("sigma.dat",ofstream::app);
+
+    sigma <<height <<"\t" <<parX[2] <<"\t" <<errparX[2] <<"\t" <<parY[2] <<"\t" <<errparY[2]  <<endl;
 
     //********************************************************************
     //*****************************DRAWING********************************
@@ -55,17 +78,19 @@ void testfit(TString dataname){
 
     c1->SetFillColor(38);
 
-    TPad *pad1=new TPad("pad1","Pad with X projection",0.03,0.03,0.48,0.97,0);
-    TPad *pad2=new TPad("pad2","Pad with Y projection",0.52,0.03,0.97,0.97,0);
+    TPad *pad1=new TPad("pad1","Pad with X projection",0.01,0.01,0.49,0.99,0);
+    TPad *pad2=new TPad("pad2","Pad with Y projection",0.51,0.01,0.99,0.99,0);
 
     pad1->Draw();
     pad2->Draw();
 
     pad1->cd();
-    gStyle->SetOptStat(0);
+    gStyle->SetOptStat(1);
+    gStyle->SetOptFit(1);
 
     TAxis *a_xXProj = h_NLaserProfile1DX->GetXaxis();
     a_xXProj->SetTitle("Column");
+    a_xXProj->SetRange(19,26);
 
     TAxis *a_yXProj = h_NLaserProfile1DX->GetYaxis();
     a_yXProj->SetTitle("Normalized V_{cal}");
@@ -74,9 +99,11 @@ void testfit(TString dataname){
 
     pad2->cd();
     gStyle->SetOptStat(0);
+    gStyle->SetOptFit(1);
 
     TAxis *a_xYProj = h_NLaserProfile1DY->GetXaxis();
     a_xYProj->SetTitle("Row");
+    a_xYProj->SetRange(40,52);
 
     TAxis *a_yYProj = h_NLaserProfile1DY->GetYaxis();
     a_yYProj->SetTitle("Normalized V_{cal}");
@@ -85,6 +112,6 @@ void testfit(TString dataname){
 
     c1->Update();
 
-    c1->SaveAs("projectionfit.pdf");
+    c1->SaveAs(dataname+".pdf");
 
 }
